a4a60f9aed30bf8a74fdba652e203d19da99d78d
TheSwerik <derswerik@gmail.com>
2020-04-27 15:21:52 +0200

    idk anymore

--- ProjectEulerWebApp-Frontend/src/karma.conf.js
+++ ProjectEulerWebApp-Frontend/src/karma.conf.js
@@ -2,6 +2,8 @@
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
 module.exports = function (config) {
+  const process = require('process');
+  process.env.CHROME_BIN = require('puppeteer').executablePath();
   config.set({
     basePath: '',
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
@@ -25,7 +27,13 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['ChromeHeadless'],
+    browsers: ['HeadlessChrome'],
+    customLaunchers:{
+      HeadlessChrome:{
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox']
+      }
+    },
     singleRun: true
   });
 };
