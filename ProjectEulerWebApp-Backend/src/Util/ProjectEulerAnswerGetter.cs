8d4ac12ac6cb12ff58dc8f14e06d4e2c4df37b01
TheSwerik <derswerik@gmail.com>
2020-05-20 15:57:12 +0200

    add TODO

--- ProjectEulerWebApp-Backend/src/Util/ProjectEulerAnswerGetter.cs
+++ ProjectEulerWebApp-Backend/src/Util/ProjectEulerAnswerGetter.cs
@@ -8,7 +8,44 @@ namespace ProjectEulerWebApp.Util
         public static Dictionary<string, long> solve(int id)
         {
             var result = new Dictionary<string, long>();
+            //TODO call the packaged Jar and Exe files
+            var java = GetJava(id);
+            if (java != -1) result.Add("Java", java);
+
+            var csharp = GetCSharp(id);
+            if (csharp != -1) result.Add("C#", csharp);
+
+            var cpp = GetCPlusPlus(id);
+            if (cpp != -1) result.Add("C++", cpp);
+
+            var python = GetPython(id);
+            if (python != -1) result.Add("Python", python);
+
             return result;
+        }
+
+        private static long GetJava(int id)
+        {
+            //TODO
+            return -1;
+        }
+
+        private static long GetCSharp(int id)
+        {
+            //TODO
+            return -1;
+        }
+
+        private static long GetCPlusPlus(int id)
+        {
+            //TODO
+            return -1;
+        }
+
+        private static long GetPython(int id)
+        {
+            //TODO
+            return -1;
         }
     }
 }
